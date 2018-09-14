6715be78f53927943ca43ce956cc03b4d99b0782
muchen <tao360907796@gmail.com>
2018-09-14 21:30:10 +0800

    feat: init java project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # next-upload-java-server
 Next-Upload Java Server Example
+
+A example code for next/upload component
+
+# next-upload-java-server
+Next-Upload Java 服务端养你代码
---
+++ src/main/java/design/fusion/javauploadserver/JavaUploadServerApplication.java
@@ -0,0 +1,12 @@
+package design.fusion.javauploadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUploadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUploadServerApplication.class, args);
+	}
+}
